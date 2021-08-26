fc76d09c801371009ca61d0e4cbb4a05a59b9ee5
AdolfoM-cyber <79242849+AdolfoM-cyber@users.noreply.github.com>
2021-08-26 14:26:16 -0700

    Enemy spawning with running animation

--- Assets/Scripts/Spawner/Enemy.cs
+++ Assets/Scripts/Spawner/Enemy.cs
@@ -6,17 +6,40 @@ public class Enemy : MonoBehaviour
 {
     public float speed;
     public string color;
+    public bool isMoving;
+    public bool hasAttack;
 
     // Start is called before the first frame update
     void Start()
     {
         
+        isMoving = true;   
     }
 
     // Update is called once per frame
     void Update()
     {
-        float frameSpeed = speed * Time.deltaTime;
-        gameObject.transform.position = new Vector3(gameObject.transform.position.x - speed, gameObject.transform.position.y, gameObject.transform.position.z); 
+        if(isMoving){
+            float frameSpeed = speed * Time.deltaTime;
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x - speed, gameObject.transform.position.y, gameObject.transform.position.z); 
+        }
     }
+
+     public void TriggerAttack() {
+        StartCoroutine("Attack");
+        //Funcion triggerea attack animation
+        //animator.SetBool("Attack", true);
+    }
+
+    private IEnumerator Attack()
+    {
+        isMoving = false;
+        hasAttack = true;
+        yield return new WaitForSeconds(1.0f);
+        isMoving = true;
+    }
+
+    void OnBecameInvisible() {
+         Destroy(gameObject);
+     }
 }
