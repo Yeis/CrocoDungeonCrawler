4c548726f18e8ad7c734e7c5587da257afe2593d
AdolfoM-cyber <79242849+AdolfoM-cyber@users.noreply.github.com>
2021-08-28 00:31:53 -0700

    Attack animation adjust to dash attack

--- Assets/Scripts/Spawner/Enemy.cs
+++ Assets/Scripts/Spawner/Enemy.cs
@@ -8,6 +8,7 @@ public class Enemy : MonoBehaviour {
     public bool isMoving;
     public bool isAttacking = false;
     public int damage = 10;
+    public float slide = 0.0037f;
 
     public Animator anim;
 
@@ -20,11 +21,11 @@ public class Enemy : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         if (isMoving) {
-            float frameSpeed = speed * Time.deltaTime;
             gameObject.transform.position = new Vector3(gameObject.transform.position.x - speed, gameObject.transform.position.y, gameObject.transform.position.z);
         }
 
         if(isAttacking){
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x - slide, gameObject.transform.position.y, gameObject.transform.position.z);
             anim.SetBool("isAttacking", true);
             StartCoroutine("Attack");
         } else {
@@ -40,8 +41,8 @@ public class Enemy : MonoBehaviour {
     private IEnumerator Attack() {
         isMoving = false;
         yield return new WaitForSeconds(1.0f);
-        isAttacking = false;
         isMoving = true;
+        isAttacking = false;
     }
     
 
