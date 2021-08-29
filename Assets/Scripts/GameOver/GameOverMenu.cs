267e99c17f02d88e1fba7122d570a5e3149517d3
AdolfoM-cyber <79242849+AdolfoM-cyber@users.noreply.github.com>
2021-08-28 22:14:58 -0700

    game over screen

--- Assets/Scripts/GameOver/GameOverMenu.cs
+++ Assets/Scripts/GameOver/GameOverMenu.cs
@@ -1,15 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class MainMenu : MonoBehaviour
+public class GameOverMenu : MonoBehaviour
 {
     public Sprite newButtonImg;
     public Button button;
-
-
     public void changeButtonImage()
     {
         button.image.sprite = newButtonImg;
--- Assets/Scripts/LevelLoader/LevelLoader.cs
+++ Assets/Scripts/LevelLoader/LevelLoader.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
     public Animator transition;
-    public float transitionTime = 1f;
+    public float transitionTime = 2.5f;
+    public string GameScene;
+    public void CouroutineStart()
+    {
+        StartCoroutine("LoadLevel");
+    }
 
-    IEnumerator LoadLevel(int levelIndex)
+    IEnumerator LoadLevel()
     {
         transition.SetTrigger("Start");
         yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(GameScene);
+        
     }
 }
